6ebb896bc90416706a8180604ee2cb863ff1f87a
Ramachandra-2k96 <149596008+Ramachandra-2k96@users.noreply.github.com>
2023-12-29 21:32:23 +0530

    updated logic for multiple apps

--- django_helper.py
+++ django_helper.py
@@ -8,23 +8,24 @@ def create_django_project(project_name,app_name):
         # Step 2: Move inside project directory
         os.chdir(project_name)
         # Step 3: Create a Django app
-        os.system(f"python manage.py startapp {app_name}")
+        os.system(f"python manage.py startapp {app_name}") 
+        setup_template(project_name,app_name)
+        setup_setting(project_name,app_name)
+        make_view(project_name,app_name)
+        setup_urls(project_name,app_name)
+        runserver()
+        print(f"Django project '{project_name}' created successfully.")
     except Exception as e:
         if os.path.exists(project_name):
             shutil.rmtree(project_name)
         print(f"Error : {e}")
-    
-    setup_template(project_name,app_name)
-    setup_setting(project_name,app_name)
-    make_view(project_name,app_name)
-    setup_urls(project_name,app_name)
-    runserver()
-    print(f"Django project '{project_name}' created successfully.")
 
 
 def setup_template(project_name,app_name):
     # Step 4: Create a templates directory
-    os.makedirs("templates")
+    os.chdir(app_name)
+    os.makedirs(f"templates/{app_name}")
+    os.makedirs(f"static/{app_name}")
     # Step 5: Create an HTML file inside the templates directory
     html = f"""
     <!DOCTYPE html>
@@ -53,21 +54,26 @@ def setup_template(project_name,app_name):
                 </p>
             </main>
         </div>
-
     </body>
     </html>
     """
-    with open(os.path.join("templates", "index.html"), "w") as html_file:
+    with open(os.path.join(f"templates/{app_name}", "index.html"), "w") as html_file:
         html_file.write(html)
         
         
 def make_view(project_name,app_name):
+    print(os.getcwd())
     # Update/create views.py and write a method to run
-    with open(os.path.join(app_name, "views.py"), "a") as file:
-        file.write("\ndef home(request):\n\treturn render(request,'index.html')")
+    with open(os.path.join(app_name,"views.py"), "a") as file:
+        file.write(f"def home(request):\n\treturn render(request,'{app_name}/index.html')")
+    with open(os.path.join(app_name,"forms.py"), "w") as file:
+        file.write(f"from django import forms\nclass inputform(forms.Form):\n\tinput=forms.CharField(label='Enter')\n")
+    with open(os.path.join(app_name,"urls.py"), "w") as file:
+        file.write(f"from django.urls import path\nfrom {app_name}.views import home\nurlpatterns = [path('',home),]")
 
 
 def setup_setting(project_name,app_name):
+    os.chdir(os.path.dirname(os.getcwd()))
     # Step 6: Update Django settings to include the app and templates
     with open(os.path.join(project_name, "settings.py"), "a") as file:
         # Make it easy for hosting
@@ -75,13 +81,11 @@ def setup_setting(project_name,app_name):
         file.write(f"\nimport os\n")
         #add installed apps
         file.write(f"\nINSTALLED_APPS += ['{app_name}',]\n")
-        #
-        file.write(f"TEMPLATES[0]['DIRS'] = [os.path.join(BASE_DIR,'templates'),]\n")
  
  
 def setup_urls(project_name,app_name):  
     with open(os.path.join(project_name, "urls.py"), "a") as file:
-        file.write(f"\nfrom {app_name}.views import home\nurlpatterns.append(path('',home))")
+        file.write(f"\nfrom django.urls import include\nurlpatterns.append(path('',include('{app_name}.urls')))")
         
      
 def runserver():
